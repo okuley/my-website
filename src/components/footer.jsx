4bb787dcd7ad3b26e549c5d95037a5a22b1485ba
Emma <baffoagyening@gmail.com>
2024-01-06 16:38:31 +0000

    first commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+  return(
+    <div className="container-fuild bg-dark">
+     
+    <p className="text-center text-white">Copyright &copy; {new Date().getFullYear()} </p>
+    <p className="text-center text-white">Emmanuel B. Gyening </p>
+    </div>
+  )
+}
+export default Footer
---
+++ src/pages/index.jsx
@@ -0,0 +1,42 @@
+function Home(){
+  return (
+    <>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+
+        <img  className="img-thumbnail img-fluid" src="./gyening.jpg"  alt="Gyening picture" />
+        </div>
+        <div className="col-6">
+        <p>
+        Hi there. I'm Emanuel Gyening. 
+        I'm a full stack developer based in Nottingham, United Kingdom.
+        I have also have techno-functional experience in Oracle Flexcube Universal Banking Software.
+        I'm well versed in programing in Java (JavaFx and Spring boot),PHP(Laravel) , React and Sql.
+        </p>
+        <p>
+          <h3>SKILLS & FOCUS</h3>
+          </p>
+       <p>Backend development</p>   
+      <p>Web development</p>
+      <p>Desktop application development</p>
+      <p>Mobile app development</p>
+      <p>
+        <h3>NETWORK</h3>
+      </p>
+      <p>
+        <a href="https://github.com/okuley">GitHub</a>
+      </p>
+      <p>
+        <a href="https://www.linkedin.com/in/emmanuel-baffour-gyening-85326013a/">Linkedin</a>
+      </p>
+      <p>
+        <a href="mailto:baffoagyening@gmail.com">baffoagyening@gmail.com</a>
+      </p>
+    </div>
+      </div>
+    </div>
+    </>
+  )
+}
+export default Home
