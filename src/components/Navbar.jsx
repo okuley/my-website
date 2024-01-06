4dfd32eed773b30cf68b1f6bd61756e49935a8e8
Emma <baffoagyening@gmail.com>
2024-01-06 19:17:08 +0000

    added /my-website to Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ function Navbar(){
   return (<div>
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">Emmanuel Baffour Gyening</a>
+    <a className="navbar-brand" href="/my-website">Emmanuel Baffour Gyening</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
